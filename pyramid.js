9ea18da2be71fde303dca463f26f71d51e23d4cd
Westindiess <gregoryalexiss@gmail.com>
2021-03-18 22:15:20 +0100

    exo cmd app

--- pyramid.js
+++ pyramid.js
@@ -1,24 +1,39 @@
-if (process.argv.length !== 4) {
-  console.log('2 parameters needed')
+
+const pyramid = (string, nbElem, reverse) => {
+  for (let i = 1; i <= nbElem; i++) {
+    if (!reverse) {
+      console.log(string.repeat(i))
+    }
+    else if (process.argv[4] === '-r') {
+      for (let i = nbElem; i >= 1; i--) {
+        console.log(string.repeat(i))
+      }
+    }
+  }
+}
+
+if (process.argv.length < 3 || process.argv.length > 5) {
+  console.log('error')
   process.exit(1)
 }
 
-if (isNaN(process.argv[3])) {
-  console.log(`Error: ${process.argv[3]} is not a number.`)
-}
-
-const pyramid = (string, nbElem) => {
-  for (let i = 1; i <= nbElem; i++) {
-    console.log(string.repeat(i))
-
-  }
-}
 
 if (!isNaN(process.argv[2])) {
   console.log(`Error: ${process.argv[2]} is not a string.`)
   process.exit(1)
 }
 
-nbStars = Number(process.argv[3])
+if (isNaN(process.argv[3])) {
+  console.log(`Error: ${process.argv[3]} is not a number.`)
+  process.exit(1)
+}
+if (!isNaN(process.argv[4])) {
+  console.log(`Error: ${process.argv[4]} is not a string.`)
+  process.exit(1)
+}
+
+
 string = process.argv[2]
-pyramid(string, nbStars)
+nbStars = Number(process.argv[3])
+r = process.argv[4]
+pyramid(string, nbStars, r)
